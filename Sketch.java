8af9d173ce9e5f158a50cfd02116033216300830
mopebad <joshmopebad@gmail.com>
2023-10-12 22:53:46 -0400

    .

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,58 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
+  int houseX, houseY; // Variables to store the house's position
+  int houseColor; // Variable to store the house's color as an integer
+  boolean houseDrawn = false; //check if house is drawn
+
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * Called once at the beginning of execution, put your size call in this method
    */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+  /**
+   * Called once at the beginning of execution. 
    */
   public void setup() {
-    background(210, 255, 173);
+    // Set the background color
+    background(220);
+
+    // Initialize the house position and color
+    houseX = (int)(random(width));
+    houseY = (int)(random(height));
+    houseColor = color(random(255), random(255), random(255));
+
+    // Display the current time in the setup
+    String currentTime = nf(hour(), 2) + ":" + nf(minute(), 2) + ":" + nf(second(), 2);
+    textSize(16);
+    fill(0);
+    text("Current Time: " + currentTime, 20, 20);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Check if the house is already drawn
+    if (!houseDrawn) {
+      // Draw the house at the randomly generated position
+      drawHouse(houseX, houseY, houseColor);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+      // House drawn
+      houseDrawn = true;
+    }
   }
-  
-  // define other methods down here.
+
+  // Draw the house
+  void drawHouse(int x, int y, int houseColor) {
+    // Draw the house body
+    fill(houseColor);
+    rect(x, y, 100, 100);
+
+    // Draw the roof
+    fill(150, 50, 0); // Brown color for the roof
+    triangle(x, y, x + 50, y - 50, x + 100, y);
+  }
 }
